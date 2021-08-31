86bed0c9e991edc92be426dd5b520ad7f6f925c2
syun-h <73629870+syun-h@users.noreply.github.com>
2021-08-31 16:09:48 +0900

    hosoinobun

---
+++ HosoiServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+//import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SaitoServlet extends HttpServlet{
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		req.setCharacterEncoding("UTF-8");
+
+        String name = req.getParameter("inputtext");
+        req.setAttribute("text", name);
+
+        RequestDispatcher dis = req.getRequestDispatcher("hosoiresult.jsp");
+        dis.forward(req, res);
+	}
+}
